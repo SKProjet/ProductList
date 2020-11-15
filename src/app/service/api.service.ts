64123e6c5cd035e615ba5b2586df9e875b3a1d00
Sandirane Kumarasamy <skumarasamy@corp.aliznet.fr>
2020-11-15 22:43:24 +0100

    add service and model

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiServer = "http://localhost:3000";
+  constructor(private httpClient: HttpClient) { }
+}
